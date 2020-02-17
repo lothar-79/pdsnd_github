0ada6758ab5051bb78fb9018a597b8b1b5324e3b
L S <l.s@gmx.net>
2020-02-17 12:43:42 +0100

    Corrections of weekdays spellings

--- bikeshare.py
+++ bikeshare.py
@@ -49,7 +49,7 @@ def get_filters():
 
 
     # get user input for day of week (all, monday, tuesday, ... sunday)
-    user_day_request = "Please choose one of the days: monday, tuesday, wednesday, thursday, friday, saturday, sunday or say all: "
+    user_day_request = "Please choose one of the days: Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday or say all: "
     day_names = ['monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday', 'sunday', 'all']
 
     while True:
